f72ca321ae408be5be7189486c57808afc899331
Muneeb <muneebshahid5@gmail.com>
2017-05-29 12:35:47 +0200

    reformated

--- tf/L2L/optimize_optimizer.py
+++ tf/L2L/optimize_optimizer.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import problems
@@ -5,7 +6,6 @@ import meta_optimizer
 import util
 from preprocess import Preprocess
 
-#
 l2l = tf.Graph()
 with l2l.as_default():
     tf.set_random_seed(20)
@@ -29,7 +29,7 @@ with l2l.as_default():
     test_loss = problem.loss(problem.variables, 'test')
 
     if flag_optimizer == 'L2L':
-        print 'Using L2L'
+        print('Using L2L')
         epochs = 10000
         num_optim_steps_per_epoch = 100
         unroll_len = 20
@@ -39,14 +39,15 @@ with l2l.as_default():
         test_epochs = 5
         num_unrolls_per_epoch = num_optim_steps_per_epoch // unroll_len
         optimizer = meta_optimizer.l2l(problem, path=None, args={'second_derivatives': second_derivatives,
-                                             'state_size': 20, 'num_layers': 2, 'unroll_len': unroll_len,
-                                             'learning_rate': 0.001,\
-                                             'meta_learning_rate': 0.01,
-                                             'preprocess': preprocess})
+                                                                 'state_size': 20, 'num_layers': 2,
+                                                                 'unroll_len': unroll_len,
+                                                                 'learning_rate': 0.001,
+                                                                 'meta_learning_rate': 0.01,
+                                                                 'preprocess': preprocess})
         loss_final, update, reset, step = optimizer.meta_minimize()
         reset = None
     else:
-        print 'Using MLP'
+        print('Using MLP')
         epochs = 500000
         num_optim_steps_per_epoch = 1
         unroll_len = 1
@@ -56,24 +57,24 @@ with l2l.as_default():
         test_epochs = 500
         num_unrolls_per_epoch = num_optim_steps_per_epoch // unroll_len
         optimizer = meta_optimizer.mlp(problem, path=load_path, args={'second_derivatives': second_derivatives,
-                                             'num_layers': 2, 'learning_rate': 0.0001, 'meta_learning_rate': 0.01,
-                                             'momentum': False, 'layer_width': 10, 'preprocess': preprocess})
+                                                                      'num_layers': 2, 'learning_rate': 0.0001,
+                                                                      'meta_learning_rate': 0.01,
+                                                                      'momentum': False, 'layer_width': 10,
+                                                                      'preprocess': preprocess})
         loss_final, update, reset, step = optimizer.meta_minimize()
         reset = None
         mean_optim_variables = [tf.reduce_mean(optimizer.w_1), tf.reduce_mean(optimizer.w_out),
                                 tf.reduce_mean(optimizer.b_1), optimizer.b_out[0][0]]
 
-
     mean_problem_variables = [tf.reduce_mean(variable) for variable in optimizer.problem.variables]
-
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         l2l.finalize()
-        print '---- Starting Training ----'
+        print('---- Starting Training ----')
         if restore_network:
             optimizer.load(sess, load_path)
-        print 'Init Problem Vars: ', sess.run(mean_problem_variables)
+        print('Init Problem Vars: ', sess.run(mean_problem_variables))
         # print 'Init Optim Vars: ', sess.run(mean_optim_variables)
         total_loss = 0
         total_time = 0
@@ -82,7 +83,7 @@ with l2l.as_default():
         best_evaluation = float("inf")
         mean_mats_values_list = list()
 
-        print '---------------------------------\n'
+        print('---------------------------------\n')
         for epoch in range(epochs):
             mean_mats_values = sess.run(mean_problem_variables)
             mean_mats_values_list.append(mean_mats_values)
@@ -90,7 +91,7 @@ with l2l.as_default():
             total_loss += loss
             total_time += time
             if (epoch + 1) % epoch_interval == 0:
-                print 'Problem Vars: ', mean_mats_values
+                print('Problem Vars: ', mean_mats_values)
                 # print 'Optim Vars: ', sess.run(mean_optim_variables)
                 log10loss = np.log10(total_loss / epoch_interval)
                 util.print_update(epoch, epochs, log10loss, epoch_interval, total_time)
@@ -100,29 +101,28 @@ with l2l.as_default():
                 mean_mats_values_list = list()
 
             if (epoch + 1) % eval_interval == 0:
-                print 'VALIDATION'
+                print('VALIDATION')
                 loss_eval_total = 0
                 for eval_epoch in range(validation_epochs):
                     time_eval, loss_eval = util.run_epoch(sess, eval_loss, None, reset, num_unrolls_per_epoch)
                     loss_eval_total += loss_eval
                 loss_eval_total = np.log10(loss_eval_total / validation_epochs)
-                print 'VALIDATION LOSS: ', loss_eval_total
+                print('VALIDATION LOSS: ', loss_eval_total)
 
-                print 'TEST'
+                print('TEST')
                 loss_test_total = 0
                 for eval_epoch in range(test_epochs):
                     time_test, loss_test = util.run_epoch(sess, test_loss, None, None, num_unrolls_per_epoch)
                     loss_test_total += loss_test
                 loss_test_total = np.log10(loss_test_total / test_epochs)
-                print 'TEST LOSS: ', loss_eval_total
+                print('TEST LOSS: ', loss_eval_total)
 
                 if save_network and loss_eval_total < best_evaluation:
-                    print 'Better Loss Found'
+                    print('Better Loss Found')
                     optimizer.save(sess, save_path + str(epoch + 1))
-                    print 'Network Saved'
+                    print('Network Saved')
                     best_evaluation = loss_eval_total
         if save_network:
             optimizer.save(sess, save_path + str(epochs) + '_FINAL')
-            print 'Final Network Saved'
-        print flag_optimizer + ' optimized.'
-
+            print('Final Network Saved')
+        print(flag_optimizer + ' optimized.')
