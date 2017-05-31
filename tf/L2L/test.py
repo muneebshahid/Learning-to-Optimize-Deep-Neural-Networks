8357d31f80412eac9ce92465275699c87c08cb89
Muneeb <muneebshahid5@gmail.com>
2017-05-31 11:56:43 +0200

    reformated

--- tf/L2L/test.py
+++ tf/L2L/test.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import problems
@@ -7,23 +8,21 @@ from preprocess import Preprocess
 
 l2l = tf.Graph()
 with l2l.as_default():
-
     tf.set_random_seed(20)
-    epochs = 50000
-    epoch_interval = 1000
+    epochs = 100
+    epoch_interval = 1
     num_optim_steps_per_epoch = 1
     unroll_len = 1
     num_unrolls_per_epoch = num_optim_steps_per_epoch // unroll_len
-    model_number = '10000'
-    load_path = 'trained_models/model_' + model_number
+    flag_optimizer = 'L2L'
+    model_id = 'model_1000000'
+    model_id += '_FINAL'
+    load_path = util.get_model_path(flag_optimizer=flag_optimizer, model_id=model_id)
     second_derivatives = False
     meta_learning_rate = 0.01
-    debug = True
-
-    optim = 'MLP'
     meta = True
 
-    problem = problems.Mnist(args={'gog': second_derivatives, 'meta': meta, 'mode': 'train'})
+    problem = problems.Mnist(args={'gog': second_derivatives, 'meta': meta, 'mode': 'test'})
     eval_loss = problem.loss(problem.variables, 'validation')
     test_loss = problem.loss(problem.variables, 'test')
 
@@ -31,16 +30,20 @@ with l2l.as_default():
     mean_optim_variables = None
     optimizer = None
     if meta:
-        if optim == 'L2L':
-            optimizer = meta_optimizer.l2l(args={'state_size': 20, 'num_layers': 2, \
-                                             'unroll_len': unroll_len, 'learning_rate': 0.001,\
-                                             'meta_learning_rate': meta_learning_rate})
-            loss_final, update, reset = optimizer.meta_loss()
+        if flag_optimizer == 'L2L':
+            optimizer = meta_optimizer.l2l(problem, path=load_path, args={'state_size': 20, 'num_layers': 2,
+                                                                          'unroll_len': unroll_len,
+                                                                          'learning_rate': 0.001,
+                                                                          'meta_learning_rate': meta_learning_rate})
+            loss_final, update, reset = optimizer.meta_step()
+            final_step = [update]
 
-        elif optim == 'MLP':
+        elif flag_optimizer == 'MLP':
             optimizer = meta_optimizer.mlp(problem, path=load_path, args={'second_derivatives': second_derivatives,
-                                                 'num_layers': 2, 'learning_rate': 0.0001, 'meta_learning_rate': 0.01,
-                                                 'momentum': False, 'layer_width': 1, 'preprocess': preprocess})
+                                                                          'num_layers': 2, 'learning_rate': 0.0001,
+                                                                          'meta_learning_rate': 0.01,
+                                                                          'momentum': False, 'layer_width': 1,
+                                                                          'preprocess': preprocess})
             loss_final, update, reset, step = optimizer.meta_minimize()
             final_step = [update, step]
             mean_optim_variables = [tf.reduce_mean(optimizer.w_1), tf.reduce_mean(optimizer.w_out),
@@ -57,55 +60,36 @@ with l2l.as_default():
 
     mean_problem_variables = [tf.reduce_mean(variable) for variable in problem.variables]
 
-
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         l2l.finalize()
-        print '---- Starting Evaluation ----'
+        print('---- Starting Evaluation ----')
         if meta:
-            print 'Resotring Optimizer'
+            print('Resotring Optimizer')
             optimizer.load(sess, load_path)
-        print 'Init Problem Vars: ', sess.run(mean_problem_variables)
+        print('Init Problem Vars: ', sess.run(mean_problem_variables))
         if mean_optim_variables is not None:
-            print 'Init Optim Vars: ', sess.run(mean_optim_variables)
+            print('Init Optim Vars: ', sess.run(mean_optim_variables))
         total_loss_final = 0
         total_loss = 0
         total_time = 0
         time = 0
         flat_grads_list, pre_pro_grads_list, deltas_list = list(), list(), list()
 
-
-        print '---------------------------------\n'
+        print('---------------------------------\n')
         for epoch in range(epochs):
             time, loss = util.run_epoch(sess, loss_final, final_step, None, num_unrolls_per_epoch)
             total_time += time
             total_loss += loss
             total_loss_final += loss
             if (epoch + 1) % epoch_interval == 0:
-                print 'Problem Vars: ', sess.run(mean_problem_variables)
+                print('Problem Vars: ', sess.run(mean_problem_variables))
                 if mean_optim_variables is not None:
-                    print 'Optim Vars: ', sess.run(mean_optim_variables)
+                    print('Optim Vars: ', sess.run(mean_optim_variables))
                 log10loss = np.log10(total_loss / epoch_interval)
                 util.print_update(epoch, epochs, log10loss, epoch_interval, total_time)
                 total_loss = 0
                 total_time = 0
                 mean_mats_values_list = list()
-            if meta and debug and epoch == 0:
-                flat_grads, pre_pro_grads, deltas = sess.run(optimizer.debug_info)
-                flatten = lambda mat_array: [element for mat in mat_array for element in mat]
-                flat_grads_list.extend(flatten(flat_grads))
-                pre_pro_grads_list.extend(flatten(pre_pro_grads))
-                deltas_list.extend(flatten(deltas))
         total_loss_final = np.log10(total_loss_final / epochs)
-        print 'Final Loss: ', total_loss_final
-        if meta and debug:
-            pre_pro_grads_array = np.array(pre_pro_grads_list)
-            flat_grads_array = np.array(flat_grads_list)
-            deltas_array = np.array(deltas_list)
-            print pre_pro_grads_array.shape
-            print flat_grads_array.shape
-            print deltas_array.shape
-            final_debug_array = np.hstack((np.hstack((pre_pro_grads_array, np.array(flat_grads_array))), np.array(deltas_array)))
-            np.savetxt('debug_' + model_number + '.txt', final_debug_array, fmt='%5f')
-
-
+        print('Final Loss: ', total_loss_final)
