eeb405373b13421e5523f69cbf0fb9c7b2c12f55
Muneeb <muneebshahid5@gmail.com>
2017-07-23 15:19:35 +0200

    working smoothed loss

--- tf/L2L/debug_optimizer.py
+++ tf/L2L/debug_optimizer.py
@@ -22,11 +22,24 @@ gs = optim_self.grad_history
 x_n = optim_self.ops_step['x_next']
 d_n = optim_self.ops_step['deltas']
 
-def itr(itera, x_s=False, g_s=False):
+def itr(itera, x_s=False, g_s=False, print_itr=1):
+
+    loss = 0
+
+    def print_loss(name, i, loss):
+        if (i + 1) % print_itr == 0:
+            print(name + ' :', np.log10(loss / print_itr))
+            return 0
+        return loss
+
     for i in range(itera):
         if x_s:
             s, u, l = iis.run([optim_self.ops_updates, optim_self.ops_step, optim_self.ops_loss])
-            print('Xloss', np.log10(l))
+            loss += l
+            loss = print_loss('Xloss', i, loss)
         if g_s:
             s, l = iis.run([optim_pre_step, optim_pre_loss])
-            print('Aloss', np.log10(l))
+            loss += l
+            loss = print_loss('Aloss', i, loss)
+
+itr(1000, x_s=True, print_itr=100)
