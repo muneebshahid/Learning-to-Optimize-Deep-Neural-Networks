4ccf1db061b0d0b06b3dd01cc94b730b9bf910ee
Muneeb <muneebshahid5@gmail.com>
2017-07-26 16:45:11 +0200

    added steps in optim

--- tf/L2L/optimize_optimizer.py
+++ tf/L2L/optimize_optimizer.py
@@ -69,7 +69,7 @@ with l2l.as_default():
     else:
         print('Using MLP')
         #########################
-        epochs = 500
+        epochs = 800
         epoch_interval = 100
         eval_interval = 200
         validation_epochs = 50
@@ -85,7 +85,7 @@ with l2l.as_default():
                                       # preprocess_args=preprocess,
                                       # learning_rate=learning_rate, layer_width=layer_width,
                                       # momentum=momentum) if restore_network else None
-        optim = meta_optimizers.MlpHistoryGradSign(problem_batches, path=io_path, args={'second_derivatives': second_derivatives,
+        optim = meta_optimizers.MlpHistoryGradNorm(problem_batches, path=io_path, args={'second_derivatives': second_derivatives,
                                                                       'hidden_layers': 1, 'learning_rate': learning_rate,
                                                                       'meta_learning_rate': 0.0001,
                                                                       'momentum': momentum, 'layer_width': layer_width,
