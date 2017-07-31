3e7ebafd1e0125393d4811235020fa45c7f2c667
Muneeb <muneebshahid5@gmail.com>
2017-07-31 17:35:21 +0200

    updated config

---
+++ tf/L2L/config.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+args = {'meta_learning_rate'}
+
+def common():
+    args = {}
+    args['meta_learning_rate'] = .0001
+    args['layer_width'] = 50
+    args['hidden_layers'] = 1
+    args['network_activation'] = tf.nn.relu
+    return args
+
+
+def norm_history():
+    args = common()
+    args['limit'] = 5
+    args['grad_only'] = True
+    args['grad_sign_only'] = False
+    args['moving_avg'] = True
+    args['network_in_dims'] = args['limit'] if args['grad_only'] else args['limit'] * 2
+    args['network_in_dims'] += (1 if args['moving_avg'] else 0)
+    args['network_out_dims'] = 12
+    return args
+
