bf38f9181caf45439e4d5e82d4861a07fa5141ab
Muneeb <muneebshahid5@gmail.com>
2017-08-02 14:29:23 +0200

    disabled moving avg

--- tf/L2L/config.py
+++ tf/L2L/config.py
@@ -15,7 +15,7 @@ def norm_history():
     args['limit'] = 5
     args['grad_only'] = True
     args['grad_sign_only'] = False
-    args['moving_avg'] = True
+    args['moving_avg'] = False
     args['network_in_dims'] = args['limit'] if args['grad_only'] else args['limit'] * 2
     args['network_in_dims'] += (1 if args['moving_avg'] else 0)
     args['network_out_dims'] = 12
