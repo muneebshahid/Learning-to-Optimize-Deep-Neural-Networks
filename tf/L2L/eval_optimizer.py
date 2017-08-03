8a0d755cdbe7092c3b3609d0706181399b97c758
Muneeb <muneebshahid5@gmail.com>
2017-08-03 17:01:41 +0200

    updated for avg log loss

--- tf/L2L/eval_optimizer.py
+++ tf/L2L/eval_optimizer.py
@@ -5,6 +5,7 @@ import problems
 import meta_optimizers
 import util
 from preprocess import Preprocess
+import config
 
 
 def write_to_file(f_name, list_var):
@@ -32,11 +33,11 @@ with l2l.as_default():
     #########################
     num_unrolls_per_epoch = 1
     io_path = util.get_model_path(flag_optimizer='Mlp', model_id=model_id)
-    problem_batches = problems.create_batches_all(train=False)
-    optim = meta_optimizers.MlpHistoryGradNorm(problem_batches, path=None,
-                                               args={'hidden_layers': 1, 'learning_rate': learning_rate,
-                                                     'meta_learning_rate': 0.0001, 'layer_width': layer_width,
-                                                     'preprocess': preprocess, 'limit': 5})
+    all_summ = []
+    writer = None
+    problem_batches, _ = problems.create_batches_all(train=True)
+    enable_summaries = False
+    optim = meta_optimizers.NormHistory(problem_batches, path=None, args=config.norm_history())
     optim.build()
 
     with tf.Session() as sess:
@@ -48,24 +49,33 @@ with l2l.as_default():
         for i, (problem, problem_variables_history) in enumerate(zip(optim.problems, optim.variable_history)):
             name_prefix = problem.__class__.__name__ + "_" + str(i)
             with tf.name_scope(name_prefix):
-                loss = tf.squeeze(tf.log(problem.loss(problem.variables)))
+                loss = tf.squeeze(problem.loss(problem.variables))
                 l.append(loss)
-                tf.summary.scalar('loss', loss)
-                for j, (variable, variable_history) in enumerate(zip(problem.variables, problem_variables_history)):
-                    # tf.summary.histogram('variable_' + str(j), variable)
-                    tf.summary.histogram('variable_history_scl_' + str(j), variable_history)
-        all_summ = tf.summary.merge_all()
-        writer = tf.summary.FileWriter(results_dir)
-        writer.add_graph(sess.graph)
+                if enable_summaries:
+                    tf.summary.scalar('loss', loss)
+                    for j, (variable, variable_history) in enumerate(zip(problem.variables, problem_variables_history)):
+                        # tf.summary.histogram('variable_' + str(j), variable)
+                        tf.summary.histogram('variable_history_scl_' + str(j), variable_history)
+        if enable_summaries:
+            all_summ = tf.summary.merge_all()
+            writer = tf.summary.FileWriter(results_dir)
+            writer.add_graph(sess.graph)
+
+
         l2l.finalize()
         print('---- Starting Evaluation ----')
-        optim.load(io_path)
+        # optim.load(io_path)
         print('Optimizer loaded.')
+        total_loss = 0
         total_itr = 10000
         for i in range(total_itr):
-            print(str(i) + '/' + str(total_itr))
             _, curr_loss, summaries = sess.run([optim.ops_updates, l, all_summ])
-            write_to_file(results_dir + 'loss', curr_loss)
-            print(curr_loss)
-            if (i + 10) % 10 == 0:
-                writer.add_summary(summaries, i)
+            total_loss += np.array(curr_loss)
+            if (i + 1) % 50 == 0:
+                print(str(i + 1) + '/' + str(total_itr))
+                avg_loss = np.log10(total_loss / 50.0)
+                write_to_file(results_dir + 'loss', avg_loss)
+                print(avg_loss)
+                total_loss = 0
+                if enable_summaries and ((i + 10) % 10 == 0):
+                    writer.add_summary(summaries, i)
