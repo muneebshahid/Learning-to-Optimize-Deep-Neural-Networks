d9988facc13bd685cdc5192df2f2c9c5db0039d2
Muneeb <muneebshahid5@gmail.com>
2017-08-28 16:11:45 +0200

    added file for plots

---
+++ tf/L2L/plot_results.py
@@ -0,0 +1,16 @@
+import numpy as np
+import glob
+from matplotlib import pyplot as plt
+import os
+
+files = glob.glob('../../../results/plots/conv/*')
+x = range(400)
+
+legends = []
+for file in files:
+    l_file = np.loadtxt(file)
+    legend, = plt.plot(x, l_file, label=os.path.basename(file))
+    legends.append(legend)
+
+plt.legend(legends)
+plt.show()
