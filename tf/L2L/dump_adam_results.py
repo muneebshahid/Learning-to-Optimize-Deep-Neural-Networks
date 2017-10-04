1cd0ab99a59b29b6af49d4007ab48c825bc09b0e
Muneeb <muneebshahid5@gmail.com>
2017-10-04 19:00:25 +0200

    added more result options

--- tf/L2L/dump_adam_results.py
+++ tf/L2L/dump_adam_results.py
@@ -3,26 +3,38 @@ import tensorflow as tf
 import numpy as np
 import time
 
-learning_rate_range = [.1, .05, .01, .005, .001, .0005, .0001, .00005, .00001]
+learning_rate_range = [#.1,
+                       #.05,
+                       # .01,
+                       # .005,
+                       # .001,
+                       # .0005,
+                       # .0001,
+                       .00005,
+                       # .00001
+                       ]
 adam_params = [[0.99, 0.9999],
-               [0.96, 0.9996],
                [0.93, 0.9993],
                [0.9, 0.999],
-               [0.86, 0.8886],
-               [0.83, 0.8883],
+               [0.85, 0.8885],
+               [0.83, 0.8885],
                [0.8, 0.888],
                [0.75, 0.7775],
                [0.7, 0.777],
                [0.65, 0.6665],
-               [0.6, 0.666]]
+               [0.6, 0.666],
+               [0.55, 0.5555],
+               [0.5, 0.555],
+]
 
 def write_to_file(f_name, list_var):
     with open(f_name, 'a') as log_file:
+
         for variable in list_var:
             log_file.write(str(variable) + ' ')
         log_file.write('\n')
 
-problem_path = '/mhome/shahidm/thesis/thesis_code/tf/L2L/mnist_save_vars_mlp/mnist_variables'
+problem_path = '/mhome/shahidm/thesis/save_nets/mnist_save_vars_mlp/mnist_variables'
 
 epochs = 50
 total_data_points = 55000
@@ -35,7 +47,7 @@ for adam_param in adam_params:
         print('================================')
         tf.reset_default_graph()
         iis = tf.InteractiveSession()
-        mnist = problems.Mnist({'minval': -100.0, 'maxval': 100.0})
+        mnist = problems.Mnist({'minval': -100.0, 'maxval': 100.0, 'conv': False, 'full': False})
         loss = tf.squeeze(mnist.loss(mnist.variables))
         mnist.restore(iis, problem_path)
         beta1 = adam_param[0]
