12abff1ffe739eef381e6a5644f8963b3f0cc654
Muneeb <muneebshahid5@gmail.com>
2017-10-05 18:11:29 +0200

    working plots

--- tf/L2L/plot_results.py
+++ tf/L2L/plot_results.py
@@ -2,14 +2,20 @@ import numpy as np
 import glob
 from matplotlib import pyplot as plt
 import os
+import ntpath
 
 files = glob.glob('../../../results/plots/conv/*')
 files = glob.glob('../../../results/plots/mlp/*')
-x = range(50)
+
+total = 50
+x = range(total)
 
 legends = []
 for file in files:
     l_file = np.loadtxt(file)
+    mean_full = "{:.5f}".format(np.mean(l_file))
+    mean_half = "{:.5f}".format(np.mean(l_file[int(total / 2):]))
+    print([ntpath.basename(file), mean_full, mean_half])
     legend, = plt.plot(x, l_file, label=os.path.basename(file))
     legends.append(legend)
 
