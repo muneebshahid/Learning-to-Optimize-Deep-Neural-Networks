42e4b0c934020569983af2efef1498263c14f0e4
Muneeb <muneebshahid5@gmail.com>
2017-10-16 20:06:30 +0200

    added optin to dump mean loss

--- tf/L2L/plot_results.py
+++ tf/L2L/plot_results.py
@@ -7,15 +7,23 @@ import ntpath
 files = glob.glob('../../../results/plots/conv/*')
 files = glob.glob('../../../results/plots/mlp/*')
 
+
+def write_to_file(f_name, list_var):
+    with open(f_name, 'a') as log_file:
+        for variable in list_var:
+            log_file.write(str(variable) + ' ')
+        log_file.write('\n')
+
 total = 50
 x = range(total)
-
 legends = []
-for file in files:
+files.sort()
+for file in reversed(files):
     l_file = np.loadtxt(file)
     mean_full = "{:.5f}".format(np.mean(l_file))
     mean_half = "{:.5f}".format(np.mean(l_file[int(total / 2):]))
     print([ntpath.basename(file), mean_full, mean_half])
+    write_to_file('lr', [mean_full])
     legend, = plt.plot(x, l_file, label=os.path.basename(file))
     legends.append(legend)
 
