4d15369335af5f840f4fd96965e6813527bdd023
Muneeb <muneebshahid5@gmail.com>
2017-11-07 17:42:45 +0100

    ploting for another funciton

--- tf/L2L/plot_lr.py
+++ tf/L2L/plot_lr.py
@@ -15,3 +15,19 @@ for global_step in range(decay_steps):
 learrning_rates = np.array(learrning_rates)
 plt.plot(range(decay_steps), learrning_rates)
 plt.show()
+
+# -decay_steps = 50000
+# -learning_rate = 0.0003
+# -end_learning_rate = 0.000005
+# +decay_steps = 500*390
+# +learning_rate = 0.05
+# +end_learning_rate = 0.0
+#  power = 4
+ 
+#  learrning_rates = []
+#  for global_step in range(decay_steps):
+# -    global_step = np.minimum(global_step, decay_steps)
+# -    decayed_learning_rate = (learning_rate - end_learning_rate) * np.power((1.0 - float(global_step) / decay_steps), power) + end_learning_rate
+# +    global_step = float(np.minimum(global_step, decay_steps))
+# +    # decayed_learning_rate = (learning_rate - end_learning_rate) * np.power((1.0 - float(global_step) / decay_steps), power) + end_learning_rate
+# +    decayed_learning_rate = end_learning_rate + 0.5 * (learning_rate - end_learning_rate) * (1 + np.cos(global_step / decay_steps) * np.pi)
